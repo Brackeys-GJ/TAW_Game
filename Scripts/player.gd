8825e6c3fc1172013519132b24e16a65e60cb15a
Sspoffy <tms032009@gmail.com>
2025-02-16 06:55:24 -0500

    2/16/25 - Update
    Added some comments and changed the map some

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,22 +1,24 @@
 extends CharacterBody2D
 
-
+#Constant Variables
 const Speed = 300.0
 const JumpVelocity = -400.0
 
-
-func _physics_process(delta: float) -> void:
-	#Movement:
+#Functions
+func _physics_process(_delta: float) -> void:
+	#X Axis Movement:
 	var XDirection := Input.get_axis("move_left", "move_right")
-	var YDirection := Input.get_axis("move_up", "move_down")
 	if XDirection:
 		velocity.x = XDirection * Speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, Speed)
-		
+	
+	#Y Axis Movement:
+	var YDirection := Input.get_axis("move_up", "move_down")
 	if YDirection:
 		velocity.y = YDirection * Speed
 	else:
 		velocity.y = move_toward(velocity.y, 0, Speed)
-
+	
+	#Apply Movement
 	move_and_slide()
