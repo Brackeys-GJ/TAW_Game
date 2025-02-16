f1f9529d2d3928fbed5fea3f493a13a36b7ed4fb
zslyv <zslyvx@proton.me>
2025-02-16 15:58:00 -0500

    Added Main Menu with keyboard controls

---
+++ Scripts/ui/BtnLogic.gd
@@ -0,0 +1,36 @@
+extends TextureButton
+
+@export var btn_text = "Placeholder"
+@export var to_scene = "path://"
+
+@onready var txt_button: RichTextLabel = $MarginContainer/TxtButton
+@onready var ptc_hover: CPUParticles2D = $MarginContainer/PtcHover
+@onready var snd_hover: AudioStreamPlayer2D = $MarginContainer/SndHover
+@onready var snd_click: AudioStreamPlayer2D = $MarginContainer/SndClick
+@onready var snd_nothing: AudioStreamPlayer2D = $MarginContainer/SndNothing
+@onready var ptc_arrow: CPUParticles2D = $MarginContainer/PtcArrow
+
+func _ready() -> void:
+	txt_button.text = btn_text
+
+func _on_pressed() -> void:
+	
+	snd_click.play()
+	get_tree().change_scene_to_file(to_scene)
+
+func _on_mouse_entered() -> void:
+	hoverBtn(snd_hover, true, "[shake rate=8.5 level=10 connected=1]%s[/shake]" % btn_text)
+
+func _on_mouse_exited() -> void:
+	hoverBtn(snd_nothing, false, btn_text)
+
+func _on_focus_entered() -> void:
+	hoverBtn(snd_hover, true, "[shake rate=8.5 level=10 connected=1]%s[/shake]" % btn_text)
+
+func _on_focus_exited() -> void:
+	hoverBtn(snd_nothing, false, btn_text)
+
+func hoverBtn(sound, particle, textButton) -> void:
+	sound.play()
+	ptc_arrow.emitting = particle
+	txt_button.text = textButton
