034c0fbf885e4c4dba08e116d56fdc16179342a5
zslyv <zslyvx@proton.me>
2025-02-17 17:53:38 -0500

    Some optimizations

--- Scripts/desktopApp.gd
+++ Scripts/desktopApp.gd
@@ -4,10 +4,15 @@ var selected = false
 var camera = inCamera
 
 @export var inCamera: Camera2D
+@export var txtFinalApp:String
+
+@onready var txt_app: RichTextLabel = $TxtApp
+@onready var area_app: Area2D = $AreaApp
 
 func _on_ready() -> void:
+	txt_app.text
 	camera = get_viewport().get_camera_2d()
-	$Area2D.input_pickable = true
+	area_app.input_pickable = true
 
 func _process(delta: float) -> void:
 	if selected:
@@ -20,11 +25,6 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 	if Input.is_action_just_pressed("click"):
 		selected = true
 		get_viewport().set_input_as_handled()
-
-#func _physics_process(delta: float) -> void:
-	#if selected:
-		#global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
-		#print("Holding")
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
