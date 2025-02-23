ee5c579fd5ee45a7ba0d458da9d00e6a1b197045
Sspoffy <tms032009@gmail.com>
2025-02-23 04:31:19 -0500

    2/23/25 - Update

--- Scripts/dialog/move_camera.gd
+++ Scripts/dialog/move_camera.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
 @export var scroll_speed: float = 10.0
-@export var max_y_position: float = 1000.0
+@export var max_y_position: float = 750.0
 
 func _process(delta: float) -> void:
 	# Move the camera down slowly, but only if it hasn't reached the max position
